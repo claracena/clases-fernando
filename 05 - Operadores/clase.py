1a9dc703d9e9edfc65b85287b6b9dc2b83297f5e
claracena <claracena@users.noreply.github.com>
2022-06-30 19:44:39 -0300

    clase 3

--- 05 - Operadores/clase.py
+++ 05 - Operadores/clase.py
@@ -3,23 +3,74 @@
 ##############
 
 # Operadores Aritmeticos
+'''
+Sumar +
+Restar -
+Multiplicar *
+Dividir /
+Modulo %
+Exponenciacion **
+Division suelo //
+abs()
+'''
+# x = 21 // 2
+# print(type(x))
 
+# print(abs(-3))
 
 # Operadores de Comparacion
-
+'''
+==  	Equal	x == y
+!=  	Not Equal	x != y
+>	    Greater Than	x > y
+<	    Less Than	x < y
+>=  	Greater than or Equal to	x >= y
+<=  	Less than or Equal to	x <= y
+'''
 
 # Operadores Logicos
-
+'''
+and 	True if both statements are true	x < 10 and x > 5
+or  	True if one statement is true	x < 10 or x > 5
+not 	False if the logical operator evaluated is true	not ( x < 10 and x > 5 )
+'''
 
 # Operadores de Asignacion
-
+'''
+=	    x = 2   	x = 2
++=  	x += 2  	x = x + 2
+-=  	x -= 2  	x = x - 2
+*=  	x *= 2  	x = x * 2
+/=  	x /= 2  	x = x / 2
+%=  	x %= 2	    x = x % 2
+//=     x //= 2	    x = x // 2
+*=* 	x **= 2	    x = x ** 2
+'''
 
 # Operadores Bitwise
-
+'''
+&	AND	x & y = 0 (AMPERSAND)
+|	OR	x | y = 10
+~	NOT	~x = -10
+^	XOR	x ^ y = 10
+>>	Right Shift	x << 2 = 10
+<<	Left Shift	x >> 2 = 150
+'''
 
 # Operadores de Membresia
-
+'''
+in  	    True if value is found in the object	    x in y
+not in  	True is value is not found in the object	x not in y
+'''
 
 # Operadores de Identidad
+'''
+is      	True if two objects are the same object	        x is x
+is not  	True if two objects are different objects	    x is y
+'''
+# x = None
+# y = None
 
+# print(id(x), id(y))
 
+# print(x is y)
--- 06 - Condicionales/clase.py
+++ 06 - Condicionales/clase.py
@@ -2,3 +2,85 @@
 # Condicionales #
 #################
 
+# dia = 'nublado'
+# estado = 'lloviendo'
+# x = 5
+
+# if dia != 'soleado':
+#     print('llevar paraguas')
+#     if estado != 'lloviendo':
+#         print('llevar botas')
+#         if x == 5:
+#             print('y x es 5')
+
+'''
+si algo es verdadero -> si esto tambien es verdadero -> si esto tambien es verdadero ...
+--------------------
+si algo es verdadero
+si algo es verdadero
+si algo es verdadero
+'''
+
+# color = input('ingrese un color: ')
+
+# if color == 'blanco':
+#     print('el color es blanco')
+# else:
+#     print('el color NO es blanco')
+
+# color = 'blanco'
+
+# if color == 'blanco':
+#     print('el color es blanco')
+# elif color == 'negro':
+#     print('el color NO es blanco. Es negro')
+# elif color == 'azul':
+#     print('el color NO es blanco. Es azul')
+# elif color == 'gris':
+#     print('el color NO es blanco. Es gris')
+# elif color == 'amarillo':
+#     print('el color NO es blanco. Es amarillo')
+# else:
+#     print('el color no es ninguno de los esperados')
+
+'''
+si algo es verdadero
+o esto otro es verdadero
+o esto otro es verdadero
+y si ninguno es verdadero.....
+'''
+
+# Ejemplo practico
+
+# edad = 12
+
+# if edad >= 13:
+#     print('bienvenido al sitio')
+# else:
+#     print('fuera de aca')
+
+# Otro ejemplo practico
+
+'''
+10 = excelente alumno
+8 - 9.9 = muy buen alumno
+6 - 7.9 = buen alumno
+4 - 5.9 = alumno regular
+0 - 3.9 = exalumno
+'''
+
+nota = 7.3
+alumno = ''
+
+if nota == 10:
+    alumno = 'excelente alumno'
+elif nota >= 8:
+    alumno = 'muy buen alumno'
+elif nota >= 6:
+    alumno = 'buen alumno'
+elif nota >= 4:
+    alumno = 'alumno regular'
+else:
+    alumno = 'exalumno'
+
+print(alumno)
